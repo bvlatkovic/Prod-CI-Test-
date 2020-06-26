a600a6500b04ca74a347259adb48a923d4baaad6
bvlatkovic <49373900+bvlatkovic@users.noreply.github.com>
2020-06-26 15:37:15 -0400

    Update create-scratch-org.sh

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -3,10 +3,3 @@ echo "Setting up DevHub Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
-# Authenticate to salesforce
-echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
-
-#Create a scratch org
-echo "Creating the Scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
