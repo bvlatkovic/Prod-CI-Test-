54ad0727ba05d1c296cdfc864c42232180a49bde
bvlatkovic <49373900+bvlatkovic@users.noreply.github.com>
2020-07-06 15:57:35 -0400

    Update and rename Authenticate-Existing-ScratchOrg.sh to authenticate-existing-scratchorg.sh

--- build/authenticate-existing-scratchorg.sh
+++ build/authenticate-existing-scratchorg.sh
@@ -1,5 +1,5 @@
 # Get the private key from the environment variable
-echo "Setting up DevHub Connection..."
+echo "Setting up ScratchOrg Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
